d37bea9bb32e01aaa99e7005fad9ed43ba78e9a7
MrAbhi04 <BittuMusicBot1@gmail.com>
2024-09-26 11:45:51 +0530

    Auto Fixes

---
+++ ABHIMUSIC/plugins/misc/autoleave.py
@@ -0,0 +1,97 @@
+
+import asyncio
+
+from pyrogram.enums import ChatType
+
+import config
+from ABHIMUSIC import app
+from ABHIMUSIC.core.call import ABHI
+from ABHIMUSIC.utils.database import (
+    get_assistant,
+    get_client,
+    is_active_chat,
+    is_autoend,
+    set_loop,
+)
+
+from .seeker import autoend
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
+            from ABHIMUSIC.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.get_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
+                        ]:
+                            chat_id = i.chat.id
+                            if chat_id not in [
+                                config.LOG_GROUP_ID,
+                                -1002159045835,
+                                -1002146211959,
+                            ]:
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(chat_id):
+                                    try:
+                                        await client.leave_chat(chat_id)
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+async def auto_end():
+    while not await asyncio.sleep(30):
+        if not await is_autoend():
+            continue
+        for chat_id in autoend:
+            count = autoend.get(chat_id)
+            if not count or count == 0:
+                try:
+                    await ABHI.stop_stream(chat_id)
+                    await set_loop(chat_id, 0)
+                    continue
+                except:
+                    continue
+            if not await is_active_chat(chat_id):
+                continue
+            userbot = await get_assistant(chat_id)
+            members = []
+            async for member in userbot.get_call_members(chat_id):
+                if member is None:
+                    try:
+                        await ABHI.stop_stream(chat_id)
+                        await set_loop(chat_id, 0)
+                        continue
+                    except:
+                        continue
+                members.append(member)
+
+            if len(members) in [0, 1]:
+                try:
+                    await ABHI.stop_stream(chat_id)
+                    await set_loop(chat_id, 0)
+                except:
+                    continue
+                try:
+                    await app.send_message(
+                        chat_id,
+                        "Bᴏᴛ ʜᴀs ʟᴇғᴛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴅᴜᴇ ᴛᴏ ɪɴᴀᴄᴛɪᴠɪᴛʏ ᴛᴏ ᴀᴠᴏɪᴅ ᴏᴠᴇʀʟᴏᴀᴅ ᴏɴ sᴇʀᴠᴇʀs. Nᴏ-ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴏɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ.",
+                    )
+                except:
+                    continue
+
+
+asyncio.create_task(auto_leave())
+asyncio.create_task(auto_end())
---
+++ ABHIMUSIC/plugins/misc/seeker.py
@@ -0,0 +1,188 @@
+
+import asyncio
+import time
+
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup
+
+from config import MUTE_WARNING_TIME
+from strings import get_string
+from ABHIMUSIC import app
+from ABHIMUSIC.core.call import ABHI
+from ABHIMUSIC.misc import db
+from ABHIMUSIC.utils.database import (
+    get_active_chats,
+    get_assistant,
+    get_cmode,
+    get_lang,
+    is_music_playing,
+    set_loop,
+)
+from ABHIMUSIC.utils.formatters import seconds_to_min
+from ABHIMUSIC.utils.inline import stream_markup_timer, telegram_markup_timer
+
+from ..admins.callback import wrong
+
+autoend = {}
+checker = {}
+mute_warnings = {}
+
+
+async def send_warning(chat_id, text):
+    chat = await app.get_chat(chat_id)
+
+    if chat.type == ChatType.CHANNEL:
+        cmode_chat_id = await get_cmode(chat_id)
+        if cmode_chat_id:
+            chat = await app.get_chat(cmode_chat_id)
+            if chat.type != ChatType.CHANNEL:
+                await app.send_message(cmode_chat_id, text)
+    else:
+        await app.send_message(chat_id, text)
+
+
+if MUTE_WARNING_TIME < 60:
+    t = f"{MUTE_WARNING_TIME} seconds"
+else:
+    t = time.strftime("%M:%S minutes", time.gmtime(MUTE_WARNING_TIME))
+
+
+async def timer():
+    while not await asyncio.sleep(1):
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if not await is_music_playing(chat_id):
+                continue
+            playing = db.get(chat_id)
+            if not playing:
+                continue
+            file_path = playing[0]["file"]
+            if "index_" in file_path or "live_" in file_path:
+                continue
+            duration = int(playing[0]["seconds"])
+            if duration == 0:
+                continue
+            db[chat_id][0]["played"] += 1
+
+
+async def process_mute_warnings():
+    while True:
+        await asyncio.sleep(2)
+        for chat_id, details in list(mute_warnings.items()):
+            if time.time() - details["timestamp"] >= MUTE_WARNING_TIME:
+                _ = details["_"]
+                try:
+                    userbot = await get_assistant(chat_id)
+                    members = []
+                    async for member in userbot.get_call_members(chat_id):
+                        if member is None:
+                            continue
+                        members.append(member)
+
+                    autoend[chat_id] = len(members)
+                    m = next((m for m in members if m.chat.id == userbot.id), None)
+                    if m is None:
+                        continue
+                    is_muted = bool(m.is_muted and not m.can_self_unmute)
+
+                    if is_muted:
+                        await ABHI.force_stop_stream(chat_id)
+                        await set_loop(chat_id, 0)
+                        await send_warning(chat_id, _["admin_35"].format(t))
+
+                    mute_warnings.pop(chat_id, None)
+                except:
+                    mute_warnings.pop(chat_id, None)
+
+
+async def markup_timer():
+    while not await asyncio.sleep(2):
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if chat_id in mute_warnings:
+                continue
+
+            try:
+                if not await is_music_playing(chat_id):
+                    continue
+                playing = db.get(chat_id)
+                if not playing:
+                    continue
+                duration_seconds = int(playing[0]["seconds"])
+                if duration_seconds == 0:
+                    continue
+                try:
+                    mystic = playing[0]["mystic"]
+                    markup = playing[0]["markup"]
+                except:
+                    continue
+                try:
+                    check = wrong[chat_id][mystic.message_id]
+                    if check is False:
+                        continue
+                except:
+                    pass
+                try:
+                    language = await get_lang(chat_id)
+                    _ = get_string(language)
+                except:
+                    _ = get_string("en")
+
+                try:
+                    userbot = await get_assistant(chat_id)
+                    members = []
+                    async for member in userbot.get_call_members(chat_id):
+                        if member is None:
+                            continue
+                        members.append(member)
+
+                    if not members:
+                        await ABHI.force_stop_stream(chat_id)
+                        await set_loop(chat_id, 0)
+                        continue
+
+                    autoend[chat_id] = len(members)
+                    m = next((m for m in members if m.chat.id == userbot.id), None)
+                    if m is None:
+                        continue
+                    is_muted = bool(m.is_muted and not m.can_self_unmute)
+
+                    if is_muted:
+                        mute_warnings[chat_id] = {
+                            "timestamp": time.time(),
+                            "_": _,
+                        }
+
+                except:
+                    continue
+
+                try:
+                    buttons = (
+                        stream_markup_timer(
+                            _,
+                            playing[0]["vidid"],
+                            chat_id,
+                            seconds_to_min(playing[0]["played"]),
+                            playing[0]["dur"],
+                        )
+                        if markup == "stream"
+                        else telegram_markup_timer(
+                            _,
+                            chat_id,
+                            seconds_to_min(playing[0]["played"]),
+                            playing[0]["dur"],
+                        )
+                    )
+                    await mystic.edit_reply_markup(
+                        reply_markup=InlineKeyboardMarkup(buttons)
+                    )
+                except:
+                    continue
+
+            except:
+                continue
+
+
+asyncio.create_task(timer())
+asyncio.create_task(markup_timer())
+asyncio.create_task(process_mute_warnings())
