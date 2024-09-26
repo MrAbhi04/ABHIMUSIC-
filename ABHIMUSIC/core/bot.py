b133f26e38d9a4c272470e307231dc9fbe24eada
Official Durgesh <durgeshgokhale656@gmail.com>
2024-09-26 12:38:13 +0530

    Auto Fixes

--- ABHIMUSIC/core/bot.py
+++ ABHIMUSIC/core/bot.py
@@ -20,7 +20,7 @@ import config
 from ..logging import LOGGER
 
 
-class ABHIBot(Client):
+class ABHIBOT(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
